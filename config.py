9b32baf7c3871ecba9140af43611f5e568bc9913
ggghhhhhhhhhhhhhhhhhhhhhhh <87254791+ggghhhhhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2025-03-13 02:13:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+from app import app, db
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
